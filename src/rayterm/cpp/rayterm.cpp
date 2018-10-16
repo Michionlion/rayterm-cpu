b0eca00f5687a35cc57a3bbd8b47c162887de9fc
Saejin Mahlau-Heinert <mahlauheinerts@allegheny.edu>
2018-10-16 16:49:13 -0400

    resizing (only down so far)

--- src/rayterm/cpp/rayterm.cpp
+++ src/rayterm/cpp/rayterm.cpp
@@ -10,15 +10,31 @@ int main (int argc, char *argv[]) {
 
 
     // do fancy hello stuff
-    wborder(stdscr, 0, 0, 0, 0, 0, 0, 0, 0);
-    move(1,1);
-    attron(A_BOLD);
-    addstr("Hello World");
-    attroff(A_BOLD);
+    wborder(WINDOWS.main, 0, 0, 0, 0, 0, 0, 0, 0);
+    wmove(WINDOWS.main, 1,1);
+    waddstr_attr(WINDOWS.main, "Hello World from the main screen", A_BOLD);
+    waddstr(WINDOWS.info, "Info Screen");
+    wmove(WINDOWS.info, 1, 0);
+    waddstr(WINDOWS.info, "And again!");
 
     // show changes
-    refresh();
-    getch();
+    allrefresh();
+
+    int key;
+    while(key = getch()) {
+        wclear(WINDOWS.info);
+        mvwprintw(WINDOWS.info, 0, 0, "KEY: %d; LINES: %d; COLS: %d", key, LINES, COLS);
+        inforefresh();
+        mainloop();
+
+        if(key == '\n') {
+            cleanup();
+            printf("Got EXIT, exiting\n");
+            break;
+        } else if (key == KEY_RESIZE) {
+            wresize(WINDOWS.main, LINES - 2, 0);
+        }
+    }
 
     // do cleanup/exit
     cleanup();
