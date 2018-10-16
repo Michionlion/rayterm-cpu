74fc849ed392214638a24c1dbcb6fd6139764886
Saejin Mahlau-Heinert <mahlauheinerts@allegheny.edu>
2018-10-16 16:48:59 -0400

    terminal updates, color, windows

--- src/rayterm/cpp/terminal.cpp
+++ src/rayterm/cpp/terminal.cpp
@@ -7,19 +7,67 @@ Windows WINDOWS;
 // Setup Terminal
 void setup() {
     initscr();
+    start_color();
     cbreak();
     noecho();
-    delwin(stdscr);
 
-    WINDOWS.main = newwin(LINES-2, 0, 0, 0);
-    WINDOWS.info = newwin(2, 0, LINES-2, 0);
+    curs_set(0);
 
-    keypad(stdscr, true);
+
+    init_pair(1,COLOR_WHITE, COLOR_BLUE);
+    init_pair(2,COLOR_WHITE, COLOR_GREEN);
+
+    WINDOWS.main = subwin(stdscr, LINES-2, 0, 0, 0);
+    WINDOWS.info = subwin(stdscr, 2, 0, LINES-2, 0);
+    wbkgd(WINDOWS.main, COLOR_PAIR(2));
+    wbkgd(WINDOWS.info, COLOR_PAIR(1));
+
+    keypad(WINDOWS.main, true);
+    keypad(WINDOWS.info, true);
+
+    allrefresh();
+}
+
+void allrefresh() {
+    inforefresh();
+    mainrefresh();
+}
+
+void inforefresh() {
+    wrefresh(WINDOWS.info);
+}
+
+void mainrefresh() {
+    wrefresh(WINDOWS.main);
+}
+
+void handle_resize() {
+    wclear(WINDOWS.main);
+    wclear(WINDOWS.info);
+
+    wmove(stdscr, 0, 0);
+    wresize(stdscr, LINES, COLS);
+    wmove(WINDOWS.main, 0, 0);
+    wresize(WINDOWS.main, LINES - 2, COLS);
+    wmove(WINDOWS.info, LINES - 2, 0);
+    wresize(WINDOWS.info, 2, COLS);
+    allrefresh();
+}
+
+void waddstr_attr(WINDOW* window, char* str, chtype attr) {
+    for(int i = 0; str[i] != '\0'; i++) {
+        waddch(window, str[i] | attr);
+    }
+}
+
+void mainloop() {
 
 }
 
 // Close down Terminal
 void cleanup() {
     std::cout << "\n";
+    delwin(WINDOWS.main);
+    delwin(WINDOWS.info);
     endwin();
 }
