277cf223caacb658a3844c473e2f6ed0f82bad3b
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2018-12-27 10:20:22 -0500

    Updated run script to handle debug/release types and gradle failures

--- run.sh
+++ run.sh
@@ -1,2 +1,26 @@
-gradle build
-./build/exe/rayterm/rayterm 2> error.log
+#!/bin/sh
+
+gradle build --console=rich
+
+if [ $? -eq 0 ]; then
+    TYPE="$1"
+    if [ -z "$TYPE" ]; then
+        TYPE="debug"
+    fi
+    rm error.log
+
+    printf "\nRunning $TYPE executable...\n"
+    sleep 0.5
+
+    #LD_LIBRARY_PATH=build/libs/rayterm/shared/$TYPE/ ./build/exe/rtexplore/$TYPE/rtexplore 2> error.log
+    ./build/exe/rtexplore//$TYPE/rtexplore 2> error.log
+
+    LOG=$(cat error.log)
+
+    if [ -z "$LOG" ]; then
+        printf "\n -- No Errors -- \n"
+    else
+        printf "\n -- Error Log -- \n"
+        echo "$LOG"
+    fi
+fi
