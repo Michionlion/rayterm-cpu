5513a3cdc614ee96c63956e169a8a938faf40d85
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-02-02 22:45:16 -0500

    Add typedefs for relevant values
    We use doubles as our scalar values, Eigen::Vector3d for 3-dimensional
    vectors, and Eigen::ParametrizedLine for rays in 3 dimensions

---
+++ src/raymath/headers/types.h
@@ -0,0 +1,10 @@
+#ifndef _RAYMATH_TYPES_H_
+#define _RAYMATH_TYPES_H_
+#include "Eigen/Dense"
+#include "Eigen/Geometry"
+
+typedef double scalar;
+typedef Eigen::Vector3d vector;
+typedef Eigen::ParametrizedLine<scalar, 3> ray;
+
+#endif
