d0f2d477d2585e0ed73684a99ab4df40b9691a66
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-02-02 22:45:50 -0500

    Add tests for sphere::intersects

--- src/raymath/test/main.cpp
+++ src/raymath/test/main.cpp
@@ -1,7 +1,20 @@
 #include <gtest/gtest.h>
 #include "sphere.h"
 
-TEST(Sphere, Intersection) {}
+TEST(Sphere, IntersectsTakesRay) {
+    sphere object(vector(4, 4, 4), 4);
+    ray light(vector(0, 0, 0), vector(1, 1, 1));
+    EXPECT_NO_THROW(object.intersects(light));
+}
+
+TEST(Sphere, RayIntersection) {
+    EXPECT_TRUE(sphere(vector(0, 0, 0), 1).intersects(ray(vector(0, 0, -3), vector(0, 0, 1))));
+    EXPECT_FALSE(sphere(vector(0, 0, 0), 1).intersects(ray(vector(0, 0, -3), vector(0, 1, 0.75))));
+    EXPECT_FALSE(sphere(vector(0, 0, 0), 1).intersects(ray(vector(0, 0, -3), vector(0, 1, 0.75))));
+    EXPECT_FALSE(sphere(vector(0, 5, 0), 2).intersects(ray(vector(0, 0, -3), vector(0, 0, 1))));
+    EXPECT_FALSE(sphere(vector(0, 5, 0), 2).intersects(ray(vector(0, 0, -3), vector(0, 0.75, 1))));
+    EXPECT_TRUE(sphere(vector(0, 5, 0), 2).intersects(ray(vector(0, 0, -3), vector(0, 1, 0.75))));
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
