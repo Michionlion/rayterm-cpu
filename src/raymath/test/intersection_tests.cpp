b8c693eddb49c0af4a09e5b37500385d8605d60a
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-02-03 01:38:16 -0500

    Add SphereRayIntersectionPosition tests for intersection

--- src/raymath/test/intersection_tests.cpp
+++ src/raymath/test/intersection_tests.cpp
@@ -31,3 +31,24 @@ TEST(Intersection, BoolEvaluation) {
         SUCCEED();
     }
 }
+
+TEST(Intersection, SphereRayIntersectionPosition) {
+    intersection hit(false);
+    vector expected;
+
+    hit      = sphere(vector(0, 0, 0), 2).intersects(ray(vector(0, 0, -3), vector(0, 0, 1)));
+    expected = vector(0, 0, -2);
+    EXPECT_TRUE(hit.position.isApprox(expected, 0.0001))
+        << "  Actual: "
+        << "{" << hit.position[0] << ", " << hit.position[1] << ", " << hit.position[2] << "}"
+        << "\nExpected: "
+        << "{" << expected[0] << ", " << expected[1] << ", " << expected[2] << "}";
+
+    hit      = sphere(vector(0, 3, 0), 2).intersects(ray(vector(0, 0, -3), vector(0, 1, 0.5)));
+    expected = vector(0, 2.27335, -1.863325);
+    EXPECT_TRUE(hit.position.isApprox(expected, 0.0001))
+        << "  Actual: "
+        << "{" << hit.position[0] << ", " << hit.position[1] << ", " << hit.position[2] << "}"
+        << "\nExpected: "
+        << "{" << expected[0] << ", " << expected[1] << ", " << expected[2] << "}";
+}
