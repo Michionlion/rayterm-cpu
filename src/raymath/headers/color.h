910626d8dc953067fcb1b8b32aec8ab182855f6a
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-02-03 16:17:19 -0500

    Add colori struct for integer colors

--- src/raymath/headers/color.h
+++ src/raymath/headers/color.h
@@ -5,4 +5,14 @@
 
 typedef Eigen::Vector3f color;
 
+struct colori {
+    int r : 8;
+    int g : 8;
+    int b : 8;
+
+    colori(color source)
+        : r(int(source[0] * 255.0)), g(int(source[1] * 255.0)), b(int(source[2] * 255.0)){};
+    colori(int r, int g, int b) : r(r), g(g), b(b){};
+};
+
 #endif
