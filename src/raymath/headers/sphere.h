af95c377f0622613f02690916c0bdf8ed44d69d8
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-02-02 22:45:34 -0500

    Add sphere definition and constructor

--- src/raymath/headers/sphere.h
+++ src/raymath/headers/sphere.h
@@ -1,7 +1,15 @@
 #ifndef _RAYMATH_SPHERE_H_
 #define _RAYMATH_SPHERE_H_
 #include "Eigen/Dense"
+#include "types.h"
 
-struct Sphere {};
+struct sphere {
+    vector center;
+    scalar radius;
+
+    sphere(vector center, scalar radius) : center(center), radius(radius){};
+
+    bool intersects(ray r);
+};
 
 #endif
