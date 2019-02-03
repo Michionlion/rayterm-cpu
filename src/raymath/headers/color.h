1df2fac7ccd10a2561f33fb4fcd460be7dae8c5f
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-02-03 17:02:52 -0500

    Fix colori struct to use correct widths and constructors types

--- src/raymath/headers/color.h
+++ src/raymath/headers/color.h
@@ -1,18 +1,21 @@
 #ifndef _RAYMATH_COLOR_H_
 #define _RAYMATH_COLOR_H_
+#include <cstdint>
 #include "Eigen/Dense"
 #include "types.h"
 
 typedef Eigen::Vector3f color;
 
 struct colori {
-    int r : 8;
-    int g : 8;
-    int b : 8;
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
 
     colori(color source)
-        : r(int(source[0] * 255.0)), g(int(source[1] * 255.0)), b(int(source[2] * 255.0)){};
-    colori(int r, int g, int b) : r(r), g(g), b(b){};
+        : r(uint8_t(source[0] * 255.0)),
+          g(uint8_t(source[1] * 255.0)),
+          b(uint8_t(source[2] * 255.0)){};
+    colori(uint8_t r, uint8_t g, uint8_t b) : r(r), g(g), b(b){};
 };
 
 #endif
