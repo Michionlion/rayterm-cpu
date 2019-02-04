eae5413bcc070bebeb65c1e4a3175b4f4ccadf7f
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-02-03 21:05:27 -0500

    Add unique_ptr as std::list elements
    This is needed to dispatch polymorphic function calls to geometry 
    instances

---
+++ src/raytrace/cpp/world.cpp
@@ -0,0 +1,13 @@
+#include "world.h"
+
+void World::intersects(ray r, intersection& record) {
+    record.hit = false;
+    intersection hit;
+    scalar closest_t = INFINITY;
+    for (geometry_list::const_iterator it = objects.cbegin(); it != objects.end(); ++it) {
+        (*it)->intersects(r, hit);
+        if (hit && hit.distance < closest_t) {
+            record = hit;
+        }
+    }
+}
--- src/raytrace/headers/world.h
+++ src/raytrace/headers/world.h
@@ -1,14 +1,21 @@
 #ifndef _RAYTERM_WORLD_H_
 #define _RAYTERM_WORLD_H_
 #include <list>
+#include <memory>
 #include "raymath"
-class World {
-    list<geometry> objects;
+
+typedef std::list<std::unique_ptr<geometry>> geometry_list;
+
+class World : geometry {
+    geometry_list objects;
 
    public:
     World(){};
 
-    void add_object(geometry& geom) { objects.push_back(geom); }
-}
+    void add_object(geometry& geom) { objects.push_back(std::unique_ptr<geometry>(&geom)); }
+
+    using geometry::intersects;
+    void intersects(ray r, intersection& record);
+};
 
 #endif
