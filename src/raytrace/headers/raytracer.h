5912f9c8af60f9312710a496833ca14e8ae58e46
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-02-04 00:36:56 -0500

    Add recursive trace function

--- src/raytrace/headers/raytracer.h
+++ src/raytrace/headers/raytracer.h
@@ -1,6 +1,10 @@
 #ifndef _RAYTERM_RAYTRACER_H_
 #define _RAYTERM_RAYTRACER_H_
+#include "raymath"
+#include "world.h"
 
 int raytrace_ppm(const char* filename);
+
+color trace(const ray& r, World& world, intersection& hit, int depth);
 
 #endif
