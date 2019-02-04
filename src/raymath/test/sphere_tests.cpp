104aed4c1a4029164d6024f4f58b6494bb6c9a71
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-02-03 19:39:55 -0500

    Add tests for overloaded geometry::intersects refactor

--- src/raymath/test/sphere_tests.cpp
+++ src/raymath/test/sphere_tests.cpp
@@ -17,3 +17,18 @@ TEST(Sphere, RayIntersection) {
     EXPECT_FALSE(sphere(vector(0, 5, 0), 2).intersects(ray(vector(0, 0, -3), vector(0, 0.75, 1))));
     EXPECT_TRUE(sphere(vector(0, 5, 0), 2).intersects(ray(vector(0, 0, -3), vector(0, 1, 0.75))));
 }
+
+TEST(Geometry, SphereGeometryInheritsOverloadedIntersects) {
+    intersection hit;
+    sphere(vector(0, 0, 0), 1).intersects(ray(vector(0, 0, -3), vector(0, 0, 1)), hit);
+    EXPECT_TRUE(hit);
+    sphere(vector(0, 0, 0), 1).intersects(ray(vector(0, 2, -3), vector(0, 0, 1)), hit);
+    EXPECT_FALSE(hit);
+
+    intersection hit2 =
+        sphere(vector(0, 0, 0), 1).intersects(ray(vector(0, 0, -3), vector(0, 0, 1)));
+    EXPECT_TRUE(hit2);
+    intersection hit3 =
+        sphere(vector(0, 0, 0), 1).intersects(ray(vector(0, 2, -3), vector(0, 0, 1)));
+    EXPECT_FALSE(hit3);
+}
