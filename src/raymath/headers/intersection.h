2c747d87f3dc7f228a32dc39027c3134e4bd5243
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-02-06 14:04:14 -0500

    Fix bug in intersection from -Wreorder

--- src/raymath/headers/intersection.h
+++ src/raymath/headers/intersection.h
@@ -11,8 +11,8 @@ struct intersection {
     scalar distance;
 
     intersection() : hit(false), id(-1){};
-    intersection(vector position, vector normal, scalar distance, long id)
-        : hit(true), position(position), normal(normal), distance(distance), id(id){};
+    intersection(long id, vector position, vector normal, scalar distance)
+        : hit(true), id(id), position(position), normal(normal), distance(distance) {};
     operator bool() const { return hit; };
     bool operator!() const { return !hit; };
 };
