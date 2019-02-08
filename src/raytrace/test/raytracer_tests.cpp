789d419e4c2e7b9fd9458f5531c74c194b9d27c1
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-02-07 20:13:53 -0500

    Add time recording to raytracer tests

--- src/raytrace/test/raytracer_tests.cpp
+++ src/raytrace/test/raytracer_tests.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include "raytracer.h"
 #include <cstdio>
+#include <chrono>
 
 void progress_cb(float progress) {
     printf("\b\b\b%02d%%", int(progress * 100));
@@ -9,7 +10,15 @@ void progress_cb(float progress) {
 TEST(RaytracerTest, Success) {
 
     printf("00%");
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
     int status = raytrace_ppm("test_image.ppm", progress_cb);
+
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
     printf("\b\b\b100% -- Render Complete!\n");
+
+    printf("Completed in %dms\n", std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count());
+
     EXPECT_EQ(raytrace_ppm("test_image.ppm"), 0) << "Did not successfully generate a test image!";
 }
