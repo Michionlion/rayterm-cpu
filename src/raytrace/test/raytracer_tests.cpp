b891fab3ce5791a95771f6699b6ef4e71958247c
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-02-10 13:38:59 -0500

    Fix misspecified flags in raytracer tests

--- src/raytrace/test/raytracer_tests.cpp
+++ src/raytrace/test/raytracer_tests.cpp
@@ -6,14 +6,14 @@
 void progress_cb(float progress) { printf("\b\b\b%02d%%", int(progress * 100)); }
 
 TEST(RaytracerTest, Success) {
-    printf("00%");
+    printf("00%%");
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
     int status = raytrace_ppm("test_image.ppm", progress_cb);
 
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
-    printf("\b\b\b100% -- Render Complete!\n");
+    printf("\b\b\b100%% -- Render Complete!\n");
 
     printf("Completed in %dms\n",
         std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count());
