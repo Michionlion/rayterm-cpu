f162f91c115bec31d5a8f9aeaca7df8f982ae202
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-02-18 00:23:26 -0500

    Add temporary tests for camera.h

---
+++ src/raytrace/test/camera_tests.cpp
@@ -0,0 +1,79 @@
+#include <gtest/gtest.h>
+#include "camera.h"
+
+TEST(Camera, AspectAndFOV) {
+    Camera* cam = new Camera(80, 40, 90);
+    //ASSERT_EQ(cam->aspect_ratio, 2);
+    //ASSERT_EQ(cam->fov_height_len, 2);
+}
+
+TEST(Camera, ToWorldDir) {
+    vector expected, actual;
+    Camera* cam = new Camera(80, 40, 90);
+
+    actual = cam->toWorldDir(vector(0, 0, -1));
+    expected = vector(0, 0, -1);
+    EXPECT_TRUE(actual.isApprox(expected, 0.0001))
+        << "  Actual: "
+        << "{" << actual[0] << ", " << actual[1] << ", " << actual[2] << "}"
+        << "\nExpected: "
+        << "{" << expected[0] << ", " << expected[1] << ", " << expected[2] << "}";
+
+    cam->look(vector(1, 0, 0));
+    actual = cam->toWorldDir(vector(0, 0, -1));
+    expected = vector(-1, 0, 0);
+    EXPECT_TRUE(actual.isApprox(expected, 0.0001))
+        << "  Actual: "
+        << "{" << actual[0] << ", " << actual[1] << ", " << actual[2] << "}"
+        << "\nExpected: "
+        << "{" << expected[0] << ", " << expected[1] << ", " << expected[2] << "}";
+
+    cam->look(vector(0, 3, -3));
+    actual = cam->toWorldDir(vector(0, 0, -1));
+    expected = vector(0, -3, 3).normalized();
+    EXPECT_TRUE(actual.isApprox(expected, 0.0001))
+        << "  Actual: "
+        << "{" << actual[0] << ", " << actual[1] << ", " << actual[2] << "}"
+        << "\nExpected: "
+        << "{" << expected[0] << ", " << expected[1] << ", " << expected[2] << "}";
+
+    cam->look(vector(1, 1, -1));
+    actual = cam->toWorldDir(vector(0, 0, 1));
+    expected = vector(-1, -1, -1).normalized();
+    EXPECT_TRUE(actual.isApprox(expected, 0.0001))
+        << "  Actual: "
+        << "{" << actual[0] << ", " << actual[1] << ", " << actual[2] << "}"
+        << "\nExpected: "
+        << "{" << expected[0] << ", " << expected[1] << ", " << expected[2] << "}";
+}
+
+TEST(Camera, ToWorldPos) {
+    vector expected, actual;
+    Camera* cam = new Camera(80, 40, 90);
+
+    actual = cam->toWorldPos(vector(0, 0, 0));
+    expected = vector(0, 0, 0);
+    EXPECT_TRUE(actual.isApprox(expected, 0.0001))
+        << "  Actual: "
+        << "{" << actual[0] << ", " << actual[1] << ", " << actual[2] << "}"
+        << "\nExpected: "
+        << "{" << expected[0] << ", " << expected[1] << ", " << expected[2] << "}";
+
+    cam->position(vector(1, 2, 3));
+    actual = cam->toWorldPos(vector(0, 0, 0));
+    expected = vector(1, 2, 3);
+    EXPECT_TRUE(actual.isApprox(expected, 0.0001))
+        << "  Actual: "
+        << "{" << actual[0] << ", " << actual[1] << ", " << actual[2] << "}"
+        << "\nExpected: "
+        << "{" << expected[0] << ", " << expected[1] << ", " << expected[2] << "}";
+
+    cam->position(vector(1, -4, 7));
+    actual = cam->toWorldPos(vector(3, 2, -2));
+    expected = vector(4, -2, 5);
+    EXPECT_TRUE(actual.isApprox(expected, 0.0001))
+        << "  Actual: "
+        << "{" << actual[0] << ", " << actual[1] << ", " << actual[2] << "}"
+        << "\nExpected: "
+        << "{" << expected[0] << ", " << expected[1] << ", " << expected[2] << "}";
+}
