47d7a50ecf69055d2f8c210d4a23635c4d82b454
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-02-19 09:39:56 -0500

    Format camera.h

--- src/raytrace/headers/camera.h
+++ src/raytrace/headers/camera.h
@@ -1,8 +1,8 @@
 #ifndef _RAYTERM_CAMERA_H_
 #define _RAYTERM_CAMERA_H_
 #include <cmath>
-#include "raymath"
 #include "Eigen/Dense"
+#include "raymath"
 
 class Camera {
     int width;
@@ -16,11 +16,12 @@ class Camera {
     Eigen::Matrix<scalar, 3, 3> camera_to_world;
 
    public:
-    Camera(int width, int height, scalar vertical_fov) : width(width), height(height), vertical_fov(vertical_fov) {
+    Camera(int width, int height, scalar vertical_fov)
+        : width(width), height(height), vertical_fov(vertical_fov) {
         aspect_ratio   = scalar(width) / scalar(height);
         fov_height_len = tan((vertical_fov / 2 * M_PI / 180));
-        origin = vector(0, 0, 0);
-        look_target = vector(0, 0, -1);
+        origin         = vector(0, 0, 0);
+        look_target    = vector(0, 0, -1);
         update_camera_matrix();
     }
 
@@ -35,16 +36,16 @@ class Camera {
     }
 
     void position_look(vector from, vector target) {
-        this->origin = from;
+        this->origin      = from;
         this->look_target = target;
         update_camera_matrix();
     }
 
     void update_camera_matrix() {
-        vector wup = vector(0, 1, 0); // must be normalized
+        vector wup     = vector(0, 1, 0);  // must be normalized
         vector forward = (origin - look_target).normalized();
-        vector right = wup.cross(forward);
-        vector up = forward.cross(right);
+        vector right   = wup.cross(forward);
+        vector up      = forward.cross(right);
 
         camera_to_world(0, 0) = right.x();
         camera_to_world(0, 1) = right.y();
@@ -60,20 +61,16 @@ class Camera {
     // get the ray originating from the camera going through screen coordinate (u, v)
     // screen coordinates are in (-1, 1)
     ray get_screen_ray(scalar u, scalar v) {
-        scalar camx = u * aspect_ratio * fov_height_len;
-        scalar camy = v * fov_height_len;
-        vector origin = toWorldPos(vector(0, 0, 0));
+        scalar camx      = u * aspect_ratio * fov_height_len;
+        scalar camy      = v * fov_height_len;
+        vector origin    = toWorldPos(vector(0, 0, 0));
         vector direction = toWorldDir(vector(camx, camy, -1).normalized());
         return ray(origin, direction);
     }
 
-    vector toWorldDir(vector direction) {
-        return camera_to_world * direction;
-    }
+    vector toWorldDir(vector direction) { return camera_to_world * direction; }
 
-    vector toWorldPos(vector position) {
-       return origin + position;
-    }
+    vector toWorldPos(vector position) { return origin + position; }
 };
 
 #endif
