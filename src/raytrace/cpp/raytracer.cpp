1f5ec51c3aa0d28ff5d6c0ea490c964b32c70b3e
Saejin Mahlau-Heinert <mahlauheinerts@allegheny.edu>
2019-02-25 18:45:07 -0500

    Revert test scene

--- src/raytrace/cpp/raytracer.cpp
+++ src/raytrace/cpp/raytracer.cpp
@@ -38,7 +38,7 @@ int raytrace_ppm(const char* filename, progress_callback cb /* = NULL */) {
 
     // geometric shape declarations
     // geometry* pointer       = new sphere(focus, 0.3);
-    geometry* ground   = new disk(vector(0, -0.5, -1.2), vector(0, 1, 0), 25);
+    geometry* ground   = new disk(vector(0, -1, -1.2), vector(0, 1, 0), 25);
     geometry* sphere1  = new sphere(vector(0, 0, -3), 1.0);
     geometry* sphere2  = new sphere(vector(2.0, -0.25, -3), 0.75);
     geometry* sphere3  = new sphere(vector(-2.75, 0.25, -3), 1.5);
@@ -54,8 +54,8 @@ int raytrace_ppm(const char* filename, progress_callback cb /* = NULL */) {
     // refractive scene tests
     geometry* sphere7 = new sphere(vector(0.75, 0, -1.5), 0.5);
     geometry* sphere8 = new sphere(vector(-0.75, 0, -1.5), 0.5);
-    // geometry* sphere9 = new sphere(vector(0, 0, -0.8), 0.5);
-    geometry* sphere9 = new sphere(vector(0, 0, -2), 0.5);
+    geometry* sphere9 = new sphere(vector(0, 0, -0.8), 0.5);
+    // geometry* sphere9 = new sphere(vector(0, 0, -2), 0.5);
 
     // // world object addition and creation
     w.add_object(new WorldObject(0, grass, &w, ground));
@@ -68,15 +68,15 @@ int raytrace_ppm(const char* filename, progress_callback cb /* = NULL */) {
 
     // w.add_object(new WorldObject(11, new Lambertian(color(0, 0, 0)), &w, pointer));
 
-    w.add_object(new WorldObject(7, new Metal(color(0.8, 0.6, 0.2), 0.25), &w, sphere7));
+    w.add_object(new WorldObject(7, new Metal(color(0.8, 0.6, 0.2), 0.6), &w, sphere7));
     // w.add_object(new WorldObject(7, new Metal(color(1, 1, 1), 0.0), &w, sphere7));
-    // w.add_object(new WorldObject(8, new Lambertian(color(0.1, 0.2, 0.5)), &w, sphere8));
+    w.add_object(new WorldObject(8, new Lambertian(color(0.1, 0.2, 0.5)), &w, sphere8));
     w.add_object(new WorldObject(9, new Dielectric(1.5), &w, sphere9));
     // w.add_object(new WorldObject(10, new Dielectric(1.5), &w, sphere10));
 
     Camera* cam = new Camera(width, height, 74);
-    // cam->position_look(vector(0, 0.5, 0.5), vector(0, 0, -1.2));
-    cam->position_look(vector(0.75, 0.5, -0.5), vector(0.75, 0, -1.5));
+    cam->position_look(vector(0, 0.5, 0.5), vector(0, 0, -1.2));
+    // cam->position_look(vector(0.75, 0.5, -0.5), vector(0.75, 0, -1.5));
 
     fprintf(outfile, "P3\n%i %i\n255\n", width, height);
     intersection hit;
